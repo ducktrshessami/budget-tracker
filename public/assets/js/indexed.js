87304a2bd826471dad19afd1cb14290e38bf2283
ducktrshessami <jwee761@gmail.com>
2021-02-11 21:25:26 -0800

    full indexed flow

--- public/assets/js/indexed.js
+++ public/assets/js/indexed.js
@@ -1,15 +1,43 @@
+const transactionName = "offline";
+
 let db;
 let connection = indexedDB.open("localTransactions");
 
-function saveRecord(transaction) {
-    
+function saveRecord(record) {
+    db.transaction(transactionName, "readwrite")
+        .objectStore(transactionName)
+        .add(record);
 }
 
 function checkDb() {
+    db.transaction(transactionName, "readwrite")
+        .objectStore(transactionName)
+        .getAll()
+        .onsuccess = function () {
+            fetch("/api/transaction/bulk", {
+                method: "post",
+                body: JSON.stringify(this.result),
+                headers: { "Content-Type": "application/json" }
+            })
+                .then(clearDb);
+        };
 
 }
 
-connection.onsuccess = function(event) {
+function clearDb() {
+    db.transaction(transactionName, "readwrite")
+        .objectStore(transactionName)
+        .clear();
+}
+
+connection.onupgradeneeded = function (event) {
+    let db = event.target.result;
+    db.createObjectStore(transactionName, { autoIncrement: true });
+};
+
+connection.onsuccess = function (event) {
     db = event.target.result;
     checkDb();
 };
+
+window.addEventListener("online", checkDb);
