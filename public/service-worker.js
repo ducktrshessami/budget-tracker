c14312698d818100fbdfd1a31fe90e08d0ce9bb0
ducktrshessami <jwee761@gmail.com>
2021-02-11 18:54:53 -0800

    list cached files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,11 @@
-const FILES = [];
+const FILES = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
 
 const STATIC_CACHE = "static-cache";
 const DATA_CACHE = "data-cache";
