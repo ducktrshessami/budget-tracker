140c9b9118d70cf6410171c5f341e76be522c22f
Josh Fish <jwee761@gmail.com>
2021-02-11 17:07:25 -0800

    handle offline cache fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,5 +12,22 @@ self.addEventListener("activate", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-    // foobar
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(caches.open(DATA_CACHE)
+            .then(cache => fetch()
+                .then(response => {
+                    if (response.status == 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(() => cache.match(event.request))
+        ));
+    }
+    else {
+        event.respondWith(caches.open(STATIC_CACHE)
+            .then(cache => cache.match(event.request))
+            .then(response => response || fetch(event.request))
+        );
+    }
 });
