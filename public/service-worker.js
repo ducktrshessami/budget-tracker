910578a1a891b2a24fd46e048520e0fe9bf54c0c
Josh Fish <jwee761@gmail.com>
2021-02-11 17:06:47 -0800

    service worker listeners

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+const FILES = [];
+
+const STATIC_CACHE = "";
+const DATA_CACHE = "";
+
+self.addEventListener("install", function(event) {
+    // foo
+});
+
+self.addEventListener("activate", function(event) {
+    // bar
+});
+
+self.addEventListener("fetch", function(event) {
+    // foobar
+});
